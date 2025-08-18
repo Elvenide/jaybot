00128bb775e1a2041f621d5cd065a9e1db07a715
Elvenide <msffodomainer@gmail.com>
2025-08-17 21:08:29 -0400

    Fix PluralKit command formatting

--- src/commands/cmd-pluralkit.ts
+++ src/commands/cmd-pluralkit.ts
@@ -6,6 +6,6 @@ new Command("pluralkit", "Explains what Pluralkit does and who it is for.")
     // Added by https://github.com/Kurumi78
     i.reply({
         ephemeral: false,
-        content: ">>> Why PluralKit? Plurality is the existence of multiple self-aware entities inside the same brain. Each member of a system/collective (group residing inside the same physical brain) is an individual person, so please treat them as such. On Discord, some systems may use PluralKit to make their message appear with the correct profile, resulting in a [BOT] tag.\n-# Note: We do NOT tolerate the use of PluralKit for role playing."
+        content: ">>> Why PluralKit? Plurality is the existence of multiple self-aware entities inside the same brain.\n\nEach member of a system/collective (group residing inside the same physical brain) is an individual person, so please treat them as such.\n\nOn Discord, some systems may use PluralKit to make their message appear with the correct profile, resulting in a [APP] tag.\n\n-# Note: We do NOT tolerate the use of PluralKit for role playing."
     });
 });
